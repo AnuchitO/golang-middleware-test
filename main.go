f720bafd3f97bae08df0fcc70c00ffd1ee640e01
Pead Nong <anuchit.prasertsang@gmail.com>
2016-01-31 17:59:29 +0700

    add makeRouter

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"log"
+	"net"
 	"net/http"
 
 	"github.com/ant0ine/go-json-rest/rest"
@@ -10,8 +11,19 @@ import (
 func main() {
 	api := rest.NewApi()
 	api.Use(rest.DefaultDevStack...)
-	api.SetApp(rest.AppSimple(func(w rest.ResponseWriter, r *rest.Request) {
-		w.WriteJson(map[string]string{"Body": "Hello World!"})
-	}))
+	router, err := rest.MakeRouter(
+		rest.Get("/", AuthenticationAPI),
+	)
+	if err != nil {
+		log.Fatal(err)
+	}
+	api.SetApp(router)
 	log.Fatal(http.ListenAndServe(":8088", api.MakeHandler()))
+}
+
+func AuthenticationAPI(w rest.ResponseWriter, req *rest.Request) {
+	ip, _ := net.LookupIP(req.PathParam("host"))
+	// rest.Error(w, err.Error(), http.StatusInternalServerError)
+	w.WriteJson(&ip)
+	w.WriteJson(map[string]interface{}{"Body": ip})
 }
