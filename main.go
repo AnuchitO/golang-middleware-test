ae16323e429385630bb9bfa363dc7465f6a2aa22
Pead Nong <anuchit.prasertsang@gmail.com>
2016-01-31 18:24:36 +0700

    add middler ware

--- main.go
+++ main.go
@@ -9,9 +9,23 @@ import (
 	"github.com/ant0ine/go-json-rest/rest"
 )
 
+type IPMiddleware struct {
+}
+
+func (ip *IPMiddleware) MiddlewareFunc(handler rest.HandlerFunc) rest.HandlerFunc {
+	fmt.Println(":: IPMiddleware ::")
+	return func(w rest.ResponseWriter, r *rest.Request) {
+		fmt.Println(":: Handler ::")
+
+		handler(w, r)
+	}
+}
+
 func main() {
+
 	api := rest.NewApi()
 	api.Use(rest.DefaultDevStack...)
+	api.Use(&IPMiddleware{})
 	router, err := rest.MakeRouter(
 		rest.Post("/", AuthenticationAPI),
 	)
